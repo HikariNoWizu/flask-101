8300ea0e8e8efcc52e2618955550b9db7369a237
Michael <michael.adragna@gmail.com>
2021-03-31 11:04:19 +0200

    Adding /api/v1/products endpoint

--- wsgi.py
+++ wsgi.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello World ff!"
+
+
+@app.route('/api/v1/products')
+def product():
+    return {
+                1: { 'id': 1, 'name': 'Skello' },
+                2: { 'id': 2, 'name': 'Socialive.tv' }
+           }
