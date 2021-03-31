1eb8574fe27cdfeda28313c32dfa7e13d1aae55e
Michael <michael.adragna@gmail.com>
2021-03-31 13:53:55 +0200

    add post delete and patch path

--- tests/test_views.py
+++ tests/test_views.py
@@ -10,5 +10,34 @@ class TestViews(TestCase):
     def test_products_json(self):
         response = self.client.get("/api/v1/products")
         products = response.json
-        self.assertIsInstance(products, dict)
+        self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_get_product(self):
+        response = self.client.get("/api/v1/products/1")
+        self.assertEquals(response.status_code, 200)
+        product = response.json
+        self.assertIsInstance(product, dict)
+        self.assertGreater(len(product), 1)
+        self.assertEquals(product, dict({'id': 1, 'name': 'Skello'}))
+
+    def test_get_product_return_404(self):
+        response = self.client.get("/api/v1/products/7")
+        self.assertEquals(response.status_code, 404)
+
+    #def test_delete_one_product(self):
+    #    response = self.client.delete("/api/v1/products/2")
+    #    self.assertEquals(response.status_code, 204)
+    # il faut le réinsérer derriere
+
+    def test_add_one_product(self):
+        response = self.client.post("/api/v1/products", json={'name': 'Netflix'})
+        self.assertEquals(response.status_code, 201)
+
+    def test_update_one_product(self):
+        response = self.client.patch("/api/v1/products", json={'id':3, 'name': 'Netflix'})
+        self.assertEquals(response.status_code, 204)
+
+    def test_update_one_product_raises_422(self):
+        response = self.client.patch("/api/v1/products", json={'id':7, 'name': 'Netflix'})
+        self.assertEquals(response.status_code, 422)
--- wsgi.py
+++ wsgi.py
@@ -1,28 +1,73 @@
 # wsgi.py
 # pylint: disable=missing-docstring
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
+import itertools
+
 app = Flask(__name__)
+
+my_dict = {
+    1: { 'id': 1, 'name': 'Skello' },
+    2: { 'id': 2, 'name': 'Socialive.tv' },
+    3: { 'id': 3, 'name': 'Socialive.tv' }
+}
 
 @app.route('/')
 def hello():
-    return "Hello World ff!"
+    return "hello world ff!"
 
 
 @app.route('/api/v1/products_old')
 def product_old():
-    return {
-                1: { 'id': 1, 'name': 'Skello' },
-                2: { 'id': 2, 'name': 'Socialive.tv' },
-                3: { 'id': 2, 'name': 'Socialive.tv' }
-           }
+    return
 
 @app.route('/api/v1/products')
-def product():
-    return jsonify( {
-                1: { 'id': 1, 'name': 'Skello' },
-                2: { 'id': 2, 'name': 'Socialive.tv' },
-                3: { 'id': 3, 'name': 'Socialive.tv' }
-           }
-           )
+def read_all_product():
+    return jsonify(list(my_dict.values()))
 
+
+@app.route('/api/v1/products/<int:id>')
+def read_one_product(id):
+    product = my_dict.get(id)
+    if product is None:
+        abort(404)
+
+    return (jsonify(product), 200)
+
+@app.route('/api/v1/products/<int:id>',methods = ['delete'])
+def delete_one_product(id):
+    product = my_dict.get(id)
+    if product is None:
+        abort(404)
+
+    del my_dict[id]
+    return (None, 204)
+
+@app.route('/api/v1/products',methods = ['post'])
+def add_one_product():
+    next_index = max(my_dict.keys()) + 1
+    my_request_json = request.get_json()
+    #print(my_request_json)
+    if 'name' not in my_request_json:
+        abort(404)
+    #print(type(my_request_json))
+    #print(my_request_json['name'])
+    my_dict[next_index] = {'id':4, 'name': my_request_json['name'] }
+    #print(my_dict)
+
+    return ("", 201)
+
+@app.route('/api/v1/products',methods = ['patch'])
+def update_one_product():
+    my_request_json = request.get_json()
+    #print(my_request_json)
+    if 'id' not in my_request_json or 'name' not in my_request_json:
+        abort(404)
+
+    if my_request_json['id'] not in my_dict:
+        abort(422)
+
+    my_dict[my_request_json['id']]['name'] = my_request_json['name']
+    print(my_dict)
+
+    return ("", 204)
