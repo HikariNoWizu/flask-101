7bcf2d73c73cfcaaf0da4dfb68d5ab39c60e2080
Michael <michael.adragna@gmail.com>
2021-03-31 11:15:33 +0200

    Add testing

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 # wsgi.py
 # pylint: disable=missing-docstring
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,9 +9,20 @@ def hello():
     return "Hello World ff!"
 
 
-@app.route('/api/v1/products')
-def product():
+@app.route('/api/v1/products_old')
+def product_old():
     return {
                 1: { 'id': 1, 'name': 'Skello' },
-                2: { 'id': 2, 'name': 'Socialive.tv' }
+                2: { 'id': 2, 'name': 'Socialive.tv' },
+                3: { 'id': 2, 'name': 'Socialive.tv' }
            }
+
+@app.route('/api/v1/products')
+def product():
+    return jsonify( {
+                1: { 'id': 1, 'name': 'Skello' },
+                2: { 'id': 2, 'name': 'Socialive.tv' },
+                3: { 'id': 3, 'name': 'Socialive.tv' }
+           }
+           )
+
